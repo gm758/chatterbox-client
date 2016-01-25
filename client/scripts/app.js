7df99cd9a8b2fb5a6c08ad20b4206e725f065d5b
gm758 <griffinmichl@gmail.com>
2016-01-25 14:38:27 -0800

    add dependencies, implement ChatterBox class

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,28 @@
-// YOUR CODE HERE:
+var ChatterBox = function() {
 
+};
+
+ChatterBox.prototype.init = function() {
+
+};
+
+ChatterBox.prototype.send = function(message) {
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+
+$(document).ready(function() {
+  window.app = new ChatterBox();
+
+});
