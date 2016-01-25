fad8ddb9cf98e10f9000a522b7c87cd71bdbcc78
gm758 <griffinmichl@gmail.com>
2016-01-25 15:44:36 -0800

    implement on submit

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,5 +1,19 @@
-var ChatterBox = function() {
+window.getQueryVariable = function(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+};
+
+
+var ChatterBox = function(username) {
   this.friendList = [];
+  this.username = username || 'anon';
+  this.currentRoom = 'lobby';
 };
 
 ChatterBox.prototype.init = function() {
@@ -8,6 +22,11 @@ ChatterBox.prototype.init = function() {
   $('body').on('click', '.username', function() {
     var friendName = $(this).text();
     app.addFriend(friendName);
+  });
+
+  $('#send .submit').off('submit').on('submit', function(event) {
+    event.preventDefault();
+    app.handleSubmit($('#message').val());
   });
 };
 
@@ -58,8 +77,18 @@ ChatterBox.prototype.addFriend = function(username) {
 
 };
 
+ChatterBox.prototype.handleSubmit = function(message) {
+  console.log('handleSubmit');
+  var messageObj = {
+    username: this.username,
+    text: message,
+    roomname: this.currentRoom
+  };
+  this.send(messageObj);
+};
+
 $(document).ready(function() {
-  window.app = new ChatterBox();
+  window.app = new ChatterBox(getQueryVariable('username'));
   app.init();
 
 });
